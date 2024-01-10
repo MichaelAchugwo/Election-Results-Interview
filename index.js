5c40c74a703485b9bcad4d439610023e2f4b5e2a
[Slingshot] <michaelachugwo69@gmail.com~>
2024-01-10 04:22:30 +0100

    done

--- index.js
+++ index.js
@@ -5,25 +5,57 @@ url = "https://upubwlugurdogswmtlom.supabase.co";
 
 database = supabase.createClient(url, key);
 
-pu_select = document.getElementById("pu-select");
+var pu_select = document.getElementById("pu-select");
+var pu_results = document.getElementById("pu-results");
 
 
 showPollingUnits = async () => {
     let announced_pu_results = await database
         .from("announced_pu_results")
-        .select("polling_unit_uniqueid, party_abbreviation, party_score");
+        .select("polling_unit_uniqueid");
 
     const uniqueIds = new Set();
 
     announced_pu_results.data.forEach(item => {
         if (!uniqueIds.has(item.polling_unit_uniqueid)) {
             const optionElement = document.createElement('option');
-            optionElement.value = item.polling_unit_uniqueid;  // Set the value attribute
-            optionElement.textContent = item.polling_unit_uniqueid;  // Set the inner text
-            pu_select.appendChild(optionElement);  // Append the option to the select element
+            optionElement.value = item.polling_unit_uniqueid; 
+            optionElement.textContent = item.polling_unit_uniqueid; 
+            pu_select.appendChild(optionElement); 
     
             uniqueIds.add(item.polling_unit_uniqueid);
         }
     });
 };
 showPollingUnits();
+
+const viewResults = async () => {
+    let currentPu = pu_select.value
+    let viewParticularResults = await database
+    .from("announced_pu_results")
+    .select("polling_unit_uniqueid, party_abbreviation, party_score");
+
+    let resultData = viewParticularResults.data;
+    const targetUniqueId = currentPu;
+    const filteredData = resultData.filter((result) => result.polling_unit_uniqueid === targetUniqueId);
+
+function createTableRow() {
+    const row = document.createElement('tr');
+    const abbreviationCell = document.createElement('td');
+    abbreviationCell.textContent = filteredData.party_abbreviation;
+    console.log(abbreviationCell)
+    row.appendChild(abbreviationCell);
+    const scoreCell = document.createElement('td');
+    scoreCell.textContent = filteredData.party_score;
+    row.appendChild(scoreCell);
+}
+function appendRowsToTable(rows) {
+    rows.forEach(row => {
+        pu_results.appendChild(row);
+    });
+}
+
+const tableRows = filteredData.map(createTableRow);
+}
+
+appendRowsToTable(tableRows);
