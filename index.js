844508d72e4bba1a190eb5a6ca2d60f567357839
[Slingshot] <michaelachugwo69@gmail.com~>
2024-01-10 02:36:56 +0100

    removed npm

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://upubwlugurdogswmtlom.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+key =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwdWJ3bHVndXJkb2dzd210bG9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MTA3NTgsImV4cCI6MjAyMDM4Njc1OH0.q_ny_P3pSAo2O3UJONmqQQ8peujO__cELkyXJJX6QTM";
 
+url = "https://upubwlugurdogswmtlom.supabase.co";
 
-let { data: lga, error } = await supabase.from('lga').select('lga_name')
+database = supabase.createClient(url, key);
+var pu_select = document.getElementById("pu-select");
 
-
-// // import mysql from 'mysql'
-// // import express from 'express'
-// const mysql = require('mysql')
-// const express = require('express')
-
-// const db = mysql.createConnection({
-//     host: "db.upubwlugurdogswmtlom.supabase.co",
-//     user: 'postgres',
-//     password: 'Clashbeast993',
-//     database: 'postgres',
-//     port: 5432
-// })
-
-// db.connect(err => {
-//     if(err) {
-//         throw err
-//     }
-//     console.log("Connection succeeded")
-// })
-
-// const app = express()
-
-// app.get('/', (res) => {
-//     let sql = "SELECT lga_name FROM lga"
-//     db.query(sql, (err) => {
-//         if(err) {
-//             throw err
-//         }
-//         res.send("Local Governments selected")
-//     })
-// })
-
-
-// // let pu_select = document.getElementById("pu-select")
-// // let option = document.createElement("option")
-// // option.clas
-
+showPollingUnits = async () => {
+    let announced_pu_results = await database
+        .from("announced_pu_results")
+        .select("polling_unit_uniqueid, party_abbreviation, party_score");
+    console.log(announced_pu_results.data.length);
+    for(i>0, i++, i<=announced_pu_results.data.length){
+      option = document.createElement('option')
+      option.innerText = `${announced_pu_results.data[i].polling_unit_uniqueid}`
+      console.log(option)
+      pu_select.appendChild = `${option}`
+      i+1
+    }
+    // var options = announced_pu_results
+    //     .map((data) => {
+    //         return "<option>" + data.item.polling_unit_uniqueid + "</option>";
+    //     })
+    //     .join(" ");
+    // console.log(options);
+    // pu_select.appendChild(options);
+};
+showPollingUnits();
